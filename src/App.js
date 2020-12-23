c9a69c0d2ac30172663b834d2e717563b4423ae5
Ben Petrosky <benpetrosky@hotmail.com>
2020-12-23 15:44:29 -0800

    Passes state to Todos Component
    renders the title of each todo item by mapping the todos array in the Todos Component

--- src/App.js
+++ src/App.js
@@ -32,12 +32,12 @@ class App extends Component {
 
 
   render(){
-    console.log(this.state.todos);
+
     return (
 
       <div className="App">
    
-        <Todos />
+        <Todos todos = {this.state.todos}/>
       </div>
     );
 

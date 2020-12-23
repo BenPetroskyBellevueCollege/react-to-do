72cda3e20afff77e0d0d4db1946b4de6bb620be2
Ben Petrosky <benpetrosky@hotmail.com>
2020-12-23 15:19:57 -0800

    Changed to Class Components
    Change App and Todos to class components that extends component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+import React, {Component} from 'react';
 import Todos from './components/Todos';
 import './App.css';
+import { render } from '@testing-library/react';
 
 
-function App() {
-  return (
-    <div className="App">
- 
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title:'Take out trash',
+        completed: false
+      
+      },
+      {
+        id: 2,
+        title:'Work on React',
+        completed: false
+      
+      },
+      {
+        id: 3,
+        title:'Apply to internships',
+        completed: false
+      
+      }
+    ]
+  }
+  
+
+
+
+  render(){
+    console.log(this.state.todos);
+    return (
+
+      <div className="App">
+   
+        <Todos />
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,16 @@
-function Todos() {
-    return (
-      <div >
-        <h1>Todos</h1>
-      </div>
-    );
+import React, {Component} from 'react';
+
+class Todos extends Component {
+
+    render(){
+        return (
+            <div >
+              <h1>Todos</h1>
+            </div>
+          );
+
+    }
+
   }
   
   export default Todos;
